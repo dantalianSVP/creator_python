ac317208ad170e5bf422f095a6bcb2ea6a97649b
Дионисий <spenceroenpro88@gmail.com>
2019-06-12 20:45:45 +0400

    tmp

---
+++ properties.py
@@ -0,0 +1,16 @@
+
+
+class Properties:
+    def __init__(self):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.email = email
+
+
+
