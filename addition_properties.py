a4d5b20a3635ab57dddca54e652a943ae603bd11
Дионисий <spenceroenpro88@gmail.com>
2019-06-13 01:58:50 +0400

    v.2 Добавлен новый объект "email3"

--- addition_properties.py
+++ addition_properties.py
@@ -1,8 +1,9 @@
 
 
 class Addition:
-    def __init__(self, address2, phone2,notes,email2 ):
+    def __init__(self, address2, phone2,notes,email2,email3):
         self.address2 = address2
         self.phone2 = phone2
         self.notes = notes
         self.email2 = email2
+        self.email3 = email3
