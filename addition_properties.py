6930cbef444653186175d39c84faf8e72ab42a1b
Дионисий <spenceroenpro88@gmail.com>
2019-06-13 00:36:56 +0400

    v.1 Рабочий класс

---
+++ addition_properties.py
@@ -0,0 +1,8 @@
+
+
+class Addition:
+    def __init__(self, address2, phone2,notes,email2 ):
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
+        self.email2 = email2
