ca9cee42a4a4e614bcc8f13d2e09579cb0e9fe88
Дионисий <spenceroenpro88@gmail.com>
2019-06-13 01:57:25 +0400

    v.1 Обработанны все поля, которые видны на главной странице в табличке -- имя, фамилия, адрес, телефоны, email-ы

--- test_add_contact.py
+++ test_add_contact.py
@@ -5,6 +5,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
 from properties import Properties
+from addition_properties import Addition
 
 class TestAddContact(unittest.TestCase):
     def setUp(self):
@@ -17,11 +18,16 @@ class TestAddContact(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_page_for_create_new_contact(wd)
-        self.filling_form_to_contact(wd, Properties(firstname= "QQQ", middlename= "WWW", lastname= "EEE",nickname="SSS",
-                                                    company="CCC",address="GGG",home="zzz",mobile="VVV",email="TTT"))
+        self.filling_form_to_contact(wd, Properties(firstname="QQQ", middlename="WWW", lastname="EEE",nickname="SSS",
+                                                    company="CCC",address="GGG", title="111", home="zzz",mobile="VVV",
+                                                    email="TTT", work="9371992", fax="123"),
+                                     Addition(email2="eee", email3="PPP",address2= "ddd",phone2="123",notes="eee"))
+
+
         self.zapolnenie_bday(wd)
         self.zapolnenie_aday(wd)
-        self.zapolnenie_dop_poley(wd)
+        self.zapolnenie_dop_poley(wd, Addition(address2="BAZOQUA", phone2="79996311111", notes="законы просты",
+                                               email2= "spenc", email3= "ssss"))
         self.sumbit_contact(wd)
         self.logout(wd)
 
@@ -31,16 +37,16 @@ class TestAddContact(unittest.TestCase):
     def sumbit_contact(self, wd):
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
-    def zapolnenie_dop_poley(self, wd):
+    def zapolnenie_dop_poley(self, wd, addition_properties):
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys("asdasd")
+        wd.find_element_by_name("address2").send_keys("%s" % addition_properties.address2)
         wd.find_element_by_name("phone2").click()
         wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys("asdasdasd")
+        wd.find_element_by_name("phone2").send_keys("%s" % addition_properties.phone2)
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys("asdasdasd")
+        wd.find_element_by_name("notes").send_keys("%s" % addition_properties.notes)
 
     def zapolnenie_aday(self, wd):
         wd.find_element_by_name("aday").click()
@@ -55,7 +61,9 @@ class TestAddContact(unittest.TestCase):
 
     def zapolnenie_bday(self, wd):
         wd.find_element_by_name("bday").click()
+        # select date of drop-list
         Select(wd.find_element_by_name("bday")).select_by_visible_text("5")
+        # transfer value "5"
         wd.find_element_by_xpath("//option[@value='5']").click()
         wd.find_element_by_name("bmonth").click()
         Select(wd.find_element_by_name("bmonth")).select_by_visible_text("October")
@@ -64,7 +72,7 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("byear").clear()
         wd.find_element_by_name("byear").send_keys("2008")
 
-    def filling_form_to_contact(self, wd, properties,):
+    def filling_form_to_contact(self, wd, properties, addition_properties):
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys("%s" % properties.firstname)
@@ -79,7 +87,7 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("nickname").send_keys("%s" % properties.nickname)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("qqq")
+        wd.find_element_by_name("title").send_keys("%s" % properties.title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
         wd.find_element_by_name("company").send_keys("%s" % properties.company)
@@ -94,19 +102,19 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("mobile").send_keys("%s" % properties.mobile)
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys("yyy")
+        wd.find_element_by_name("work").send_keys("%s" % properties.work)
         wd.find_element_by_name("fax").click()
         wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys("uuu")
+        wd.find_element_by_name("fax").send_keys("%s" % properties.fax)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
         wd.find_element_by_name("email").send_keys("%s" % properties.email)
         wd.find_element_by_name("email2").click()
         wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys("rqrqrq")
+        wd.find_element_by_name("email2").send_keys("%s" % addition_properties.email2)
         wd.find_element_by_name("email3").click()
         wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys("qrqrqr")
+        wd.find_element_by_name("email3").send_keys("%s" % addition_properties.email3)
         wd.find_element_by_name("homepage").click()
         wd.find_element_by_name("homepage").clear()
         wd.find_element_by_name("homepage").send_keys("qwqw")
