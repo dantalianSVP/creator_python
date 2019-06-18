0a58d08b3f7d98396f29730477eaa738411c5c71
Дионисий <spenceroenpro88@gmail.com>
2019-06-18 19:19:42 +0400

    v.1 Рабочий класс

--- properties.py
+++ properties.py
@@ -2,7 +2,7 @@
 
 class Properties:
 
-    def __init__(self, firstname,middlename,lastname,nickname,company,address,home,mobile,email ):
+    def __init__(self, firstname,middlename,lastname,nickname,company,address,home,mobile,email,title,work,fax ):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -12,6 +12,10 @@ class Properties:
         self.home = home
         self.mobile = mobile
         self.email = email
+        self.title = title
+        self.work = work
+        self.fax = fax
+
 
 
 
