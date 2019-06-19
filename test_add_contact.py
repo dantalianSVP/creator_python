9dafed22954f9b31e513da9970af17d3120c77d6
Дионисий <spenceroenpro88@gmail.com>
2019-06-19 20:19:06 +0400

    v.2 Перевел на pytest

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,150 +1,29 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+
+import pytest
 from properties import Properties
 from addition_properties import Addition
+from fixture_cont import Application
 
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+@pytest.fixture
+def app(request):
+    fuxture = Application()
+    request.addfinalizer(fuxture.destroy)
+    return fuxture
 
-    # test-case one
-    def test_add_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_page_for_create_new_contact(wd)
-        self.filling_form_to_contact(wd, Properties(firstname="QQQ", middlename="WWW", lastname="EEE",nickname="SSS",
+
+# test-case one
+def test_add_contact(app):
+        app.open_home_page()
+        app.login(username="admin", password="secret")
+        app.open_page_for_create_new_contact()
+        app.filling_form_to_contact(Properties(firstname="QQQ", middlename="WWW", lastname="EEE",nickname="SSS",
                                                     company="CCC",address="GGG", title="111", home="zzz",mobile="VVV",
-                                                    email="TTT", work="9371992", fax="123", title='EEERRR'),
+                                                    email="TTT", work="9371992", fax="123"),
                                      Addition(email2="eee", email3="PPP",address2= "ddd",phone2="123",notes="eee"))
-
-
-        self.zapolnenie_bday(wd)
-        self.zapolnenie_aday(wd)
-        self.zapolnenie_dop_poley(wd, Addition(address2="BAZOQUA", phone2="79996311111", notes="законы просты",
+        app.zapolnenie_bday()
+        app.zapolnenie_aday()
+        app.zapolnenie_dop_poley(Addition(address2="BAZOQUA", phone2="79996311111", notes="законы просты",
                                                email2= "spenc", email3= "ssss"))
-        self.sumbit_contact(wd)
-        self.logout(wd)
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def sumbit_contact(self, wd):
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-
-    def zapolnenie_dop_poley(self, wd, addition_properties):
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys("%s" % addition_properties.address2)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys("%s" % addition_properties.phone2)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys("%s" % addition_properties.notes)
-
-    def zapolnenie_aday(self, wd):
-        wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text("12")
-        wd.find_element_by_xpath("(//option[@value='12'])[2]").click()
-        wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text("May")
-        wd.find_element_by_xpath("(//option[@value='May'])[2]").click()
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys("2009")
-
-    def zapolnenie_bday(self, wd):
-        wd.find_element_by_name("bday").click()
-        # select date of drop-list
-        Select(wd.find_element_by_name("bday")).select_by_visible_text("5")
-        # transfer value "5"
-        wd.find_element_by_xpath("//option[@value='5']").click()
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("October")
-        wd.find_element_by_xpath("//option[@value='October']").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("2008")
-
-    def filling_form_to_contact(self, wd, properties, addition_properties):
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("%s" % properties.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("%s" % properties.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("%s" % properties.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("%s" % properties.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("%s" % properties.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("%s" % properties.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("%s" % properties.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("%s" % properties.home)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("%s" % properties.mobile)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys("%s" % properties.work)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys("%s" % properties.fax)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("%s" % properties.email)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys("%s" % addition_properties.email2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys("%s" % addition_properties.email3)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys("qwqw")
-
-    def open_page_for_create_new_contact(self, wd):
-        wd.find_element_by_link_text("add new").click()
-
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("%s" % username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("%s" % password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+        app.sumbit_contact()
+        app.logout()
