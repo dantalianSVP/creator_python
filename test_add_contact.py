6105f4b5b14a513080b7362403d421203b9b60bd
Дионисий <spenceroenpro88@gmail.com>
2019-06-19 19:35:08 +0400

    v.2 Добавил параметр из вспомогательного класса

--- test_add_contact.py
+++ test_add_contact.py
@@ -20,7 +20,7 @@ class TestAddContact(unittest.TestCase):
         self.open_page_for_create_new_contact(wd)
         self.filling_form_to_contact(wd, Properties(firstname="QQQ", middlename="WWW", lastname="EEE",nickname="SSS",
                                                     company="CCC",address="GGG", title="111", home="zzz",mobile="VVV",
-                                                    email="TTT", work="9371992", fax="123"),
+                                                    email="TTT", work="9371992", fax="123", title='EEERRR'),
                                      Addition(email2="eee", email3="PPP",address2= "ddd",phone2="123",notes="eee"))
 
 
