eb901ba1b1fb832a181f18add320526833fdae77
Дионисий <spenceroenpro88@gmail.com>
2019-06-23 13:11:18 +0400

    Выделен класс для управления сессиями

--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from fixture.session import SessionHelper
 
 
 class Application:
@@ -6,12 +7,9 @@ class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
+        self.session = SessionHelper(self)
 
     # ______Вспомогательные методы
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
     def return_group_page(self,):
         wd = self.wd
@@ -39,16 +37,6 @@ class Application:
         wd = self.wd
         wd.find_element_by_link_text("groups").click()
 
-    def Login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("%s" % username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("%s" % password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self,):
         wd = self.wd
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,18 +13,18 @@ def app(request):
 # ____Start test case
 
 def test_add_group(app):
-    app.Login(username="admin", password="secret")
+    app.session.Login(username="admin", password="secret")
     app.creation_group(Group(name="FTP", header="gasd", footer="qed"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_group(app):
-    app.Login(username="admin", password="secret")
+    app.session.Login(username="admin", password="secret")
     app.creation_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_one_group(app):
-    app.Login(username="admin", password="secret")
+    app.session.Login(username="admin", password="secret")
     app.creation_group(Group(name="123", header="", footer=" "))
-    app.logout()
+    app.session.logout()
 
 # ____ End test case
