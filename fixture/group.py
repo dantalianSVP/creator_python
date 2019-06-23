0cf5277a4d454e59c657a7914c247fa7dbae3acb
Дионисий <spenceroenpro88@gmail.com>
2019-06-23 14:57:15 +0400

    Добавлена кейс удаления группы

--- fixture/group.py
+++ fixture/group.py
@@ -28,6 +28,16 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_group_page()
 
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.Open_group_page()
+        #select first group
+        wd.find_element_by_name("selected[]").click()
+        #submit
+        wd.find_element_by_name("delete").click()
+        self.return_group_page()
+
     def Open_group_page(self,):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.group import Group
-from fixture.application import Application
 
 
 @pytest.fixture
