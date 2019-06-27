b6d9bab639b30f906bfa031716d93fa21c0f62f6
dantalianSVP <spenceronepro88@gmail.com>
2019-06-27 09:46:05 +0300

    Доработал редактирование группы

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,6 +1,7 @@
+from model.group import Group
 
 
 def test_edit_group(app):
     app.session.Login(username="admin", password="secret")
-    app.group.edit_group()
+    app.group.edit_group(Group(name="SLAYER", header="SLAYER", footer="SLAYER"))
     app.session.logout()
