d1aeaf174d115a6ee2c5815f5e718c687d4cd216
Дионисий <spenceroenpro88@gmail.com>
2019-06-29 14:29:25 +0400

    Переделан кейс изменения

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -6,5 +6,6 @@ def test_edit_contact(app):
     app.delcont.edit_contact(Properties(firstname="DIMITRASH",
                                         middlename="DIO", lastname="Dmitr", nickname="DLN",
                                         company="Sccc",address="KIBA", title="PARAVOZ",home="977777",
-                                        mobile="44444",email="33333", work="321", fax="3213321321"))
+                                        mobile="44444",email="33333", work="321", fax="3213321321",
+                                        address2="PAPA_ROA",phone2="SALY",notes="MOON",email2 = "NIRVANA",email3="PE"))
     app.session.logout()
