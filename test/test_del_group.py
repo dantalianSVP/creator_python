70179cadd72817dcff3edbdc55fb1f92201cdb08
Дионисий <spenceroenpro88@gmail.com>
2019-06-29 14:54:34 +0400

    Переделан метод удаления/редактирования

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -2,5 +2,5 @@
 
 def test_delete_first_group(app):
     app.session.Login(username="admin", password="secret")
-    app.group.delete_first_group()
+    app.modifygroup.delete_first_group()
     app.session.logout()
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,5 +3,5 @@ from model.group import Group
 
 def test_edit_group(app):
     app.session.Login(username="admin", password="secret")
-    app.group.edit_group(Group(name="SLAYER", header="SLAYER", footer="SLAYER"))
+    app.modifygroup.edit_group(Group(name="SLAYER", header="SLAYER", footer="SLAYER"))
     app.session.logout()
