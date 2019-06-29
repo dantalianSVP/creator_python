4da9bafaf3ca99e47d2de7b5b566232ff4f79369
Дионисий <spenceroenpro88@gmail.com>
2019-06-29 14:54:12 +0400

    Добавлен новый хелпер с модификацией группы

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 
 from fixture.delcont import ModifyContact
+from fixture.modifygroup import ModifyGroup
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
@@ -14,6 +15,7 @@ class Application:
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
         self.delcont = ModifyContact(self)
+        self.modifygroup = ModifyGroup(self)
 
 
 
