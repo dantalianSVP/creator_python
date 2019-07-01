19b059d919301b78ec61399a0157b13dbf2f93f2
Дионисий <spenceroenpro88@gmail.com>
2019-07-01 22:56:18 +0400

    Добавлены проверки if (пока не работает надо разобрать)

--- fixture/group.py
+++ fixture/group.py
@@ -16,20 +16,23 @@ class GroupHelper:
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
-        self.fill_form_group(group, wd)
+        self.fill_form_group(group)
         # submit group
         wd.find_element_by_name("submit").click()
         self.return_group_page()
 
-    def fill_form_group(self, group, wd):
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("%s" % group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("%s" % group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("%s" % group.footer)
+    def fill_form_group(self, group,):
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, text, field_name):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys("%s" % text)
 
     def Open_group_page(self,):
         wd = self.app.wd
@@ -39,18 +42,35 @@ class GroupHelper:
     def delete_first_group(self):
         wd = self.app.wd
         self.Open_group_page()
-        #select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         #submit
         wd.find_element_by_name("delete").click()
         self.return_group_page()
 
-    def edit_group(self, group):
+    def select_first_group(self):
+        # select first group
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
+    def modify_first_group(self, new_group_data):
+        wd = self.app.wd
+        self.Open_group_page()
+        self.select_first_group()
+        wd.find_element_by_xpath("//input[@name='edit']").click()
+        self.fill_form_group(new_group_data)
+        wd.find_element_by_name("update").click()
+        self.return_group_page()
+
+
+
+
+
+    def edit_group(self, new_group_data):
         wd = self.app.wd
         self.Open_group_page()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath("//input[@name='edit']").click()
-        self.fill_form_group(group, wd)
+        self.fill_form_group(new_group_data)
         wd.find_element_by_name("update").click()
         self.return_group_page()
 
