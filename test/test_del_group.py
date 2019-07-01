2a34e8bd993898090d021bca63b0f7db10aee8ff
Дионисий <spenceroenpro88@gmail.com>
2019-07-01 21:27:41 +0400

    Актуализация теста групп

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -2,5 +2,5 @@
 
 def test_delete_first_group(app):
     app.session.Login(username="admin", password="secret")
-    app.modifygroup.delete_first_group()
+    app.group.delete_first_group()
     app.session.logout()
