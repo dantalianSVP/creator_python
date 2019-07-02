c31fe775cbd347a1a7f5ef8457096c866a0ee308
Дионисий <spenceroenpro88@gmail.com>
2019-07-02 22:49:47 +0400

    Добавлены проверки перед удалением

--- fixture/group.py
+++ fixture/group.py
@@ -13,26 +13,25 @@ class GroupHelper:
     def create(self, group):
         wd = self.app.wd
         self.Open_group_page()
-        # init group creation
         wd.find_element_by_name("new").click()
-        # fill group form
         self.fill_form_group(group)
-        # submit group
         wd.find_element_by_name("submit").click()
         self.return_group_page()
 
-    def fill_form_group(self, group):
-        #wd = self.app.wd
-        self.change_field_value("group_name", group.name)
-        self.change_field_value("group_header", group.header)
-        self.change_field_value("group_footer", group.footer)
-
-    def change_field_value(self,field_name, text):
+    def change_field_value(self, field_name, text):
         wd = self.app.wd
         if text is not None:
             wd.find_element_by_name(field_name).click()
             wd.find_element_by_name(field_name).clear()
             wd.find_element_by_name(field_name).send_keys("%s" % text)
+
+    def fill_form_group(self, group):
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+
 
     def Open_group_page(self,):
         wd = self.app.wd
@@ -74,3 +73,7 @@ class GroupHelper:
         wd.find_element_by_name("update").click()
         self.return_group_page()
 
+    def count(self):
+        wd = self.app.wd
+        self.Open_group_page()
+        return len(wd.find_elements_by_name("selected[]"))
