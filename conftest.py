389fad0dd85ffd7ac5f8c0b2a37255bcfffe983b
dantalianSVP <spenceronepro88@gmail.com>
2019-07-02 16:55:16 +0300

    Добавлен ителектуальный логин

--- conftest.py
+++ conftest.py
@@ -10,11 +10,10 @@ def app(request):
     global fixture
     if fixture is None:
         fixture = Application()
-        fixture.session.Login(username="admin", password="secret")
     else:
         if not fixture.is_valid():
             fixture = Application()
-            fixture.session.Login(username="admin", password="secret")
+    fixture.session.ensure_Login(username="admin", password="secret")
     return fixture
 
 
@@ -22,7 +21,7 @@ def app(request):
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
-        fixture.session.logout()
+        fixture.session.ensure_logout()
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
