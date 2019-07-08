4180e27eb6873cc975899c3a8320f8bba972face
Spenc <spenceronepro88@gmail.com>
2019-07-08 21:41:03 +0300

    Доработал проверки в группах

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,8 +3,12 @@ from model.group import Group
 
 def test_modify_group(app):
     old_groups = app.group.get_group_list()
+    group = Group(name="New Group")
+    group.id = old_groups[0].id
     if app.group.count() == 0:
         app.group.create(Group(name="PAPARIZI", header="PAPARIZI", footer="PAPARIZI"))
-    app.group.modify_first_group(Group(name="New Group"))
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
