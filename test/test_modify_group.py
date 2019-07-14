eec6d9f32767e79c2cea4c3934a18402c5e9c127
Spenc <spenceronepro88@gmail.com>
2019-07-14 19:13:34 +0300

    Реализованны случайности в группах

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,14 +1,15 @@
 from model.group import Group
-
+from random import randrange
 
 def test_modify_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="New Group")
-    group.id = old_groups[0].id
     if app.group.count() == 0:
-        app.group.create(Group(name="PAPARIZI", header="PAPARIZI", footer="PAPARIZI"))
-    app.group.modify_first_group(group)
+        app.group.create(Group(name="FTP", header="gasd", footer="qed"))
+    old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
+    group = Group(name="New Group")
+    group.id = old_groups[index].id
+    app.group.modify_group_by_index(index, group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == app.group.count()
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
