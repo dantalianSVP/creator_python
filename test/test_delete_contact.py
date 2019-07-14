a6f828e6b99f27cffa44433a6440f480dba96979
Spenc <spenceronepro88@gmail.com>
2019-07-14 17:51:25 +0300

    Рабочее сравнение после удаления

--- test/test_delete_contact.py
+++ test/test_delete_contact.py
@@ -7,4 +7,10 @@ def test_delete_contact(app):
                                                     company="CCC",address="GGG", title="111", home="zzz",mobile="VVV",
                                                     email="TTT", work="9371992", fax="123",
                                      email2="eee", email3="PPP",address2= "ddd",phone2="123",notes="eee",homepage='TATATA'))
+    old_clist = app.contact.get_contact_list()
     app.contact.delete_first_contact()
+    new_clist = app.contact.get_contact_list()
+    assert len(old_clist) -1 == len(new_clist)
+    old_clist[0:1] = []
+    assert old_clist == new_clist
+
