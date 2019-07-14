d8bfd8779d18a9da7daa945f07c0bdb22410e740
Spenc <spenceronepro88@gmail.com>
2019-07-14 18:12:58 +0300

    Реализованна проверка кеширования и хеширования

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -9,6 +9,6 @@ def test_modify_group(app):
         app.group.create(Group(name="PAPARIZI", header="PAPARIZI", footer="PAPARIZI"))
     app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+    assert len(old_groups) == app.group.count()
     old_groups[0] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
