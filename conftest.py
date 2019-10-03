4aaaef9c4028100e10c66bc2e0a5e944698f6e17
dantalianSVP <51408238+dantalianSVP@users.noreply.github.com>
2019-10-03 19:33:23 +0300

    Реализованна загрузка информации о тестируемом приложении из файла

--- conftest.py
+++ conftest.py
@@ -1,10 +1,10 @@
 import pytest
 from fixture.application import Application
 import json
+import os.path
 
 fixture = None
 target = None
-
 
 
 @pytest.fixture
@@ -13,19 +13,21 @@ def app(request):
     global target
     browser = request.config.getoption("--browser")
     if target is None:
-        with open(request.config.getoption("--target")) as config_file:
-            target = json.load(config_file)
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as c:
+            target = json.load(c)
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=target['baseUrl'])
     fixture.session.ensure_Login(username=target['username'], password=target['password'])
     return fixture
 
 
-@pytest.fixture(scope="session",autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def stop(request):
     def fin():
         fixture.session.ensure_logout()
         fixture.destroy()
+
     request.addfinalizer(fin)
     return fixture
 
