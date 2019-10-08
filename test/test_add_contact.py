4085b801533eb300fd781a480968ee6474200bb9
dantalianSVP <51408238+dantalianSVP@users.noreply.github.com>
2019-10-08 10:34:21 +0300

    Сравнение контактов из БД

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,17 +3,12 @@ from model.properties import Properties
 import pytest
 
 
-
-
-
-
-
-def test_add_contact(app, json_contacts):
+def test_add_contact(app, db, json_contacts):
         contact = json_contacts
-        old_clist = app.contact.get_contact_list()
+        old_clist = db.get_contact_list()
         app.contact.create_cont(contact)
         app.contact.Open_home_page()
-        new_clist = app.contact.get_contact_list()
+        new_clist = db.get_contact_list()
         assert len(old_clist) + 1 == len(new_clist)
         old_clist.append(contact)
         assert sorted(old_clist, key=Properties.id_or_max) == sorted(new_clist, key=Properties.id_or_max)
